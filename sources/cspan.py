64172c8df8ee6d5389db6737ebed7865844f4c8a
davy <davy.wybiral@gmail.com>
2019-09-28 18:17:07 -0500

    added cspan

---
+++ sources/cspan.py
@@ -0,0 +1,56 @@
+import aiohttp
+from bs4 import BeautifulSoup
+from cache import Cache
+from sources import BaseSource
+
+class Source(BaseSource):
+
+    name = 'C-SPAN'
+    url = 'https://www.c-span.org'
+    cache_size = 500
+
+    def __init__(self, module):
+        self.logfile = 'logs/%s.txt' % module
+        self.cache = Cache(size=self.cache_size)
+        self.cache.load(self.logfile)
+
+    async def update(self, queue):
+        headers = {'User-Agent': 'Firehose'}
+        async with aiohttp.ClientSession(headers=headers) as s:
+            url = 'https://www.c-span.org/search/'
+            query = 'searchtype=Videos&sort=Most+Recent+Airing&ajax&page=1'
+            async with s.get(url + '?' + query) as r:
+                text = await r.text()
+                soup = BeautifulSoup(text, 'html.parser')
+                videos = soup.find_all('li', {'class': 'onevid'})
+                for video in reversed(videos):
+                    await self._update_video(queue, video)
+        self.cache.save(self.logfile)
+
+    async def _update_video(self, queue, video):
+        text = video.find('div', {'class': 'text'})
+        url = text.find('a', {'class': 'title'})['href']
+        if url in self.cache:
+            return
+        x = {}
+        text = video.find('div', {'class': 'text'})
+        title_a = text.find('a', {'class': 'title'})
+        url = title_a['href']
+        x['url'] = url
+        x['title'] = title_a.find('h3').get_text()
+        dates = text.find_all('time')
+        date = ''
+        if len(dates) > 0 and 'datetime' not in dates[-1]:
+            date = dates[-1]['datetime']
+        x['date'] = date
+        body_p = text.find('p', {'class': 'abstract'})
+        if body_p:
+            x['body'] = body_p.get_text()
+        thumb_a = video.find('a', {'class': 'thumb'})
+        x['thumb'] = thumb_a.find_all('img')[0]['src']
+        x['source'] = {
+            'name': self.name,
+            'url': self.url,
+        }
+        self.cache.add(url)
+        await queue.put(x)
