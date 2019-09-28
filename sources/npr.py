932e0c51ddd41bb1ef9809b1370712b091c6132d
davy <davy.wybiral@gmail.com>
2019-09-28 18:41:50 -0500

    added npr

---
+++ sources/npr.py
@@ -0,0 +1,55 @@
+import aiohttp
+from bs4 import BeautifulSoup
+from cache import Cache
+from sources import BaseSource
+
+class Source(BaseSource):
+
+    name = 'NPR News'
+    url = 'https://www.npr.org/sections/news/'
+    cache_size = 500
+
+    def __init__(self, module):
+        self.logfile = 'logs/%s.txt' % module
+        self.cache = Cache(size=self.cache_size)
+        self.cache.load(self.logfile)
+        self.feeds = [
+            'https://www.npr.org/feeds/1006/feed.json',
+            'https://www.npr.org/feeds/1014/feed.json',
+            'https://www.npr.org/feeds/1007/feed.json',
+            'https://www.npr.org/feeds/1019/feed.json',
+            'https://www.npr.org/feeds/1004/feed.json',
+            'https://www.npr.org/feeds/1001/feed.json',
+        ]
+
+    async def update(self, queue):
+        headers = {'User-Agent': 'Firehose'}
+        async with aiohttp.ClientSession(headers=headers) as s:
+            for url in self.feeds:
+                async with s.get(url) as r:
+                    json = await r.json()
+                    for item in json['items']:
+                        await self._update_item(queue, item)
+        self.cache.save(self.logfile)
+
+    async def _update_item(self, queue, item):
+        url = item['url']
+        url, _ = url.split('?', 1)
+        if url in self.cache:
+            return
+        x = {}
+        x['url'] = url
+        date = item['date_published']
+        date = date[:19].replace('T', ' ')
+        x['date'] = date
+        x['title'] = item['title']
+        if 'summary' in item:
+            x['body'] = item['summary']
+        if 'image' in item:
+            x['thumb'] = item['image']
+        x['source'] = {
+            'name': self.name,
+            'url': self.url,
+        }
+        self.cache.add(url)
+        await queue.put(x)
