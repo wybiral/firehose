e52baec1d28a4bf1f05fb7c930a7e6b30f460fbd
davy <davy.wybiral@gmail.com>
2019-09-28 19:35:05 -0500

    added threatpost

---
+++ sources/threatpost.py
@@ -0,0 +1,8 @@
+from sources import RSSSource
+
+class Source(RSSSource):
+    name = 'Threatpost'
+    url = 'https://threatpost.com/'
+    feeds = [
+        'https://threatpost.com/feed/',
+    ]
