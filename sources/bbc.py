b8f5881323801bcaf15fc694f23dff2dc95f8d67
davy <davy.wybiral@gmail.com>
2020-02-12 09:22:03 -0600

    remove bbc/sport

--- sources/bbc.py
+++ sources/bbc.py
@@ -9,3 +9,8 @@ class Source(RSSSource):
         ('http://feeds.bbci.co.uk/news/world/rss.xml', 'world'),
         'http://feeds.bbci.co.uk/news/rss.xml',
     ]
+
+    def map(self, x):
+        if x['url'].startswith('https://www.bbc.co.uk/sport/'):
+            return None
+        return x
